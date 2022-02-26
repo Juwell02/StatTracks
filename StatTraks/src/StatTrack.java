c3c532e08de37cf4854207f67f9a4d6e3e9698c5
Juwell02 <email2justinw@gmail.com>
2022-02-25 20:36:23 -0600

    I think I finally have this thing working correctly!

--- StatTraks/src/StatTrack.java
+++ StatTraks/src/StatTrack.java
@@ -4,6 +4,7 @@ public class StatTrack {
 	public static void main(String[] args) {
 			System.out.println("Final Countdown");
 			System.out.println("Horay");
+			System.out.println("Finally!!");
 	}
 
 }
